bb34df5d536a7b54a2a8c9d2828cf7abd772ca0c
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-30 13:10:14 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -6,24 +6,24 @@ async function getData(){
      console.log(characterArr)
      const characterArrSliced = characterArr.slice(0,6);
      character.results.forEach(element => {
-        const getData = (min, max) => {
-            return Math.floor(Math.random() * (min - max + 1)+ max)
-        }
-        const randIndex = getData(1, characterArr.length);
+        const randomData = (min, max) => Math.floor(Math.random() * max - min +1) + min;
+        const randInt = randomData(1, characterArr.length); 
+        const randIndex = randInt;
         for(i = 0; i < characterArrSliced.length; i++){
             if(element.id == i){
                 const box = document.createRange().createContextualFragment(`
                     
-                    <div class="grid-box box-${i} "><a href="" class="fa fa-star fa-2x"></a>
-                        <img src="${characterArr[randIndex][8][1]} alt="">
+                    <div class="grid-box box-${i}"><a href="" class="fa fa-star fa-2x"></a>
+                         <img src="${characterArr[randIndex][8][1]}" alt="">
                     </div>
                     
                     `)
                     const stories = document.getElementById('stories');
                     stories.append(box)
+                }
             }
-        }
-     });
+            console.log(characterArr[randIndex][8][1])
+        });
 }
 
 getData()
