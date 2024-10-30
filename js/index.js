c4ad41b949444babdc6dee2efc94ba4be2271d59
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-30 16:36:56 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -26,4 +26,45 @@ async function getData(){
         });
 }
 
+const btn_validar = document.getElementById('btn-validar');
+function validar(e){
+e.preventDefault()
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const mensaje = document.getElementById('mensaje');
+const arr = [];
+const messageArr = ["Nombre", "Email", "Mensaje"];
+arr.push(nombre, email, mensaje);
+for(i = 0; i < arr.length; i++){
+    if(arr[i].value == ""){
+        swal({
+            title: `El campo ${messageArr[i]} no puede estar vacío`,
+            icon: "error",
+             })
+             return false;
+    }
+}
+if(!emailValido(email)){
+    swal({
+        title: `El campo ${messageArr[1]} no tiene el formato correcto`,
+        icon: "error",
+         })
+         return false;
+}
+swal({
+    title: `Datos enviados satisfactoriamente`,
+    icon: "success",
+     })
+     nombre.value = "";
+     email.value = "";
+     mensaje.value = "";
+     return true;
+
+}
+const emailValido = (email) => {
+return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)
+}
+
+btn_validar.addEventListener("click", validar)
+
 getData()
